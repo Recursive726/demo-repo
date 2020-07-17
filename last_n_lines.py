710be5b3b13d79cb4f6635e8c3c0794da431bcb0
Recursive726 <abdev.zewia@gmail.com>
2020-07-17 20:47:13 +0530

    second commit

--- last_n_lines.py
+++ last_n_lines.py
@@ -1,6 +1,7 @@
 def file_read(fname):
         with open(fname) as f:
-                #Content_list is the list that contains the read lines.     
+                #Content_list is the list that contains the read lines. 
+                # this is my udpdated line 
                 content_list = f.readlines()
                 print(content_list)
 
